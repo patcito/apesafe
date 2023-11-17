18d1eb48855e1da746da39f17ac3b9f5df63394e
Patrick Aljord <patcito@gmail.com>
2023-11-17 20:15:52 +0300

    initial commit

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,5 +1,15 @@
-import type { OnRpcRequestHandler } from '@metamask/snaps-types';
-import { panel, text } from '@metamask/snaps-ui';
+import type {
+  ChainId,
+  OnRpcRequestHandler,
+  OnTransactionHandler,
+} from '@metamask/snaps-types';
+import { heading, panel, text } from '@metamask/snaps-ui';
+import { createPublicClient, http } from 'viem';
+import { mainnet } from 'viem/chains';
+import { ethers } from 'ethers';
+import { ChainIds, detectAddress, detectProtocol } from './protocols';
+import { parse } from 'path';
+import { getChainId } from 'viem/_types/actions/public/getChainId';
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -30,4 +40,55 @@ export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
     default:
       throw new Error('Method not found.');
   }
+};
+
+export const onTransaction: OnTransactionHandler = async ({
+  transaction,
+  chainId,
+  transactionOrigin,
+}) => {
+  const provider = new ethers.BrowserProvider(ethereum);
+
+  const currentGasPrice = await ethereum.request({
+    method: 'eth_gasPrice',
+  });
+
+  const response = await fetch('https://beaconcha.in/api/v1/execution/gasnow');
+  const insights = [
+    {
+      value: 'This is a transaction insight',
+      transaction: JSON.stringify(transaction),
+      transactionOrigin: JSON.stringify(transactionOrigin),
+      chainId: JSON.stringify(chainId) as ChainIds,
+    },
+  ];
+
+  const protocol = detectProtocol(transactionOrigin || '');
+  const txTo = transaction.to as string;
+  const snapChainId = insights[0]?.chainId as ChainId;
+  const parsedChainId = snapChainId.split(':')[1]?.replace('"', '') as ChainIds;
+  console.log('insights chainid', chainId);
+  console.log('snap chainid', snapChainId);
+  console.log('parsed chainid', parsedChainId);
+
+  const contract = detectAddress(txTo, parsedChainId, protocol?.name || '');
+
+  const contractName = contract?.name === 'unknown' ? txTo : contract?.name;
+  return {
+    content: panel([
+      heading('My Transaction Insights'),
+      text('Here are the insights:'),
+      /*      ...insights.map((insight) => text('value: ' + insight.value)),
+      ...insights.map((insight) => text('tx: ' + insight.transaction)),
+      ...insights.map((insight) =>
+        text('tx origin: ' + insight.transactionOrigin),
+      ),
+      ...insights.map((insight) => text('chainId:' + insight.chainId)),
+      ...insights.map((insight) => text('currentGasPrice')),
+      ...insights.map((insight) => text(currentGasPrice)),
+      ...insights.map((insight) => text('blockNumber: ' + response.text)),*/
+      ...insights.map((insight) => text(`protocol: ${protocol?.name} ✅`)),
+      ...insights.map((insight) => text(`contract: ${contractName} ✅`)),
+    ]),
+  };
 };
